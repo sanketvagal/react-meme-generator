99b16f9f342adaa9a775e3dec8bafd4b24ea1756
sanketvagal <sanketdvagal@gmail.com>
2023-07-10 21:59:38 -0300

    Added button to create randomised url, data file

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme(props) {
+
+    const randoMeme = () => {
+        let memes = memesData.data.memes
+        console.log(memes[Math.floor(Math.random() * memes.length)].url)
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" placeholder="Top text" className="form--input" />
                 <input type="text" placeholder="Bottom text" className="form--input" />
-                <button className="form--button">Get a new image 😜</button>
-            </form>
+                <button onClick={randoMeme} className="form--button">Get a new image 😜</button>
+            </div>
         </main>
     )
 
