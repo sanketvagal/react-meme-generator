7d1003643748d34aafd1dcb34355fdad9e60cdc1
sanketvagal <sanketdvagal@gmail.com>
2023-07-12 19:20:51 -0300

    Added text CSS to meme component images, simple form tutorials

---
+++ src/assets/Simpleform2.js
@@ -0,0 +1,97 @@
+import React from "react"
+
+export default function App() {
+
+    /**
+     * Challenge: Connect the form to local state
+     * 
+     * 1. Create a state object to store the 4 values we need to save.
+     * 2. Create a single handleChange function that can
+     *    manage the state of all the inputs and set it up
+     *    correctly
+     * 3. When the user clicks "Sign up", check if the 
+     *    password & confirmation match each other. If
+     *    so, log "Successfully signed up" to the console.
+     *    If not, log "passwords to not match" to the console.
+     * 4. Also when submitting the form, if the person checked
+     *    the "newsletter" checkbox, log "Thanks for signing
+     *    up for our newsletter!" to the console.
+     */
+    const [formData, setFormData] = React.useState(
+        {
+            email: "",
+            password: "",
+            confpass: "",
+            news: ""
+        })
+    // console.log(formData)    
+    function handleChange(event) {
+        const { name, value, type, checked } = event.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: type === "checkbox" ? checked : value
+            }
+        })
+    }
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(formData)
+        if (formData.password != formData.confpass) {
+            console.log("passwords to not match")
+        } else {
+            console.log("Successfully signed up")
+            if (formData.news) {
+                console.log("Thanks for signing up for our newsletter!")
+            }
+        }
+    }
+
+    return (
+        <div className="form-container">
+            <form className="form" onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    placeholder="Email address"
+                    onChange={handleChange}
+                    className="form--input"
+                    value={formData.email}
+                    name="email"
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                    className="form--input"
+                    value={formData.password}
+                    name="password"
+                />
+                <input
+                    type="password"
+                    placeholder="Confirm password"
+                    onChange={handleChange}
+                    className="form--input"
+                    value={formData.confpass}
+                    name="confpass"
+                />
+
+                <div className="form--marketing">
+                    <input
+                        id="okayToEmail"
+                        type="checkbox"
+                        checked={formData.news}
+                        name="news"
+                        onChange={handleChange}
+
+                    />
+                    <label htmlFor="okayToEmail">I want to join the newsletter</label>
+                </div>
+                <button
+                    className="form--submit"
+                >
+                    Sign up
+                </button>
+            </form>
+        </div>
+    )
+}
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,21 +3,13 @@ import memesData from "../memesData";
 import { useState } from "react";
 
 /**
- * Challenge: Update our state to save the meme-related
- * data as an object called `meme`. It should have the
- * following 3 properties:
- * topText, bottomText, randomImage.
- * 
- * The 2 text states can default to empty strings for now,
- * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
- * 
- * Next, create a new state variable called `allMemeImages`
- * which will default to `memesData`, which we imported above
- * 
- * Lastly, update the `getMemeImage` function and the markup 
- * to reflect our newly reformed state object and array in the
- * correct way.
- */
+    * Challenge: 
+    * 1. Set up the text inputs to save to
+    *    the `topText` and `bottomText` state variables.
+    * 2. Replace the hard-coded text on the image with
+    *    the text being saved to state.
+    */
+
 export default function Meme(props) {
 
     const [meme, setMemeImage] = useState({
@@ -34,14 +26,23 @@ export default function Meme(props) {
         setMemeImage(prevMeme => ({ ...prevMeme, randomImage: memeUrl }))
     }
 
+    function handleChange(event) {
+        const { name, value } = event.target
+        setMemeImage(prevMeme => ({ ...prevMeme, [name]: value }))
+    }
+    console.log(meme)
     return (
         <main>
             <div className="form">
-                <input type="text" placeholder="Top text" className="form--input" />
-                <input type="text" placeholder="Bottom text" className="form--input" />
+                <input type="text" placeholder="Top text" className="form--input" onChange={handleChange} name="topText" value={meme.topText} />
+                <input type="text" placeholder="Bottom text" className="form--input" onChange={handleChange} name="bottomText" value={meme.bottomText} />
                 <button onClick={getMemeImage} className="form--button">Get a new image 😜</button>
             </div>
-            <img src={meme.randomImage} className="meme--image" />
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 
