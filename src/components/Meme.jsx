e8e3526771275581775b3546d705275ab9e0b706
sanketvagal <sanketdvagal@gmail.com>
2023-07-12 23:26:16 -0300

    Fixed image center alignment

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,16 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-/**
-useEffect takes a function as its parameter. If that function
-returns something, it needs to be a cleanup function. Otherwise,
-it should return nothing. If we make it an async function, it
-automatically retuns a promise instead of a function or nothing.
-Therefore, if you want to use async operations inside of useEffect,
-you need to define the function separately inside of the callback
-function, as seen below:
-*/
-
 export default function Meme(props) {
 
     const [meme, setMemeImage] = useState({
